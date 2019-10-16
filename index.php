47b36618cb5dec1e6db468809dc674edc43d7aa0
cheikh3008 <cheikh3008@gmail.com>
2019-10-16 15:54:29 +0000

    page d'accueil

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/main_index.css"> 
+    
+    
+    <title>ACCUEIL</title>
+</head>
+<body>
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+        <img src="img/slider-image1.jpg" class="d-block w-100" alt="Image de fond" width="100%" height="auto">
+    </div>
+    <div class="content">
+       <nav>
+             <ul>
+                <li><a class="lien" href="pages/admin/admin.php">Compte Administrateur      </a></li>
+                <li><a class="lien" href="pages/secretaire/secretaire.php">Compte Secretaire</a></li>
+                <li><a class="lien" href="pages/medecin/medecin.php">Compte Medecin         </a></li>
+            </ul>
+       </nav>
+    </div>
+
+</body>
+</html>
