7d75c9bc3d956880b3d3653f97267a24a812dcaf
cheikh3008 <cheikh3008@gmail.com>
2019-10-19 17:14:31 +0000

    supression de fichier des requettes

--- pages/secretaire/mes_req_sql.sql
+++
@@ -1,2 +0,0 @@
-afficher tous les patients ajouter par secretaire = ?
-SELECT DISTINCT patient.prenom,patient.nom,patient.age,patient.telephone,patient.adresse, secretaire.prenom as 'prenom secretaire',secretaire.nom as 'nom secretaire' FROM secretaire,patient WHERE secretaire.id_secretaire = 1  AND patient.id_secretaire = 1;
