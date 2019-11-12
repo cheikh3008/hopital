d3397fd4989ce30081b951462c654d2bf5d21d4d
cheikh3008 <cheikh3008@gmail.com>
2019-11-12 19:42:09 +0000

    Creation page modification pour medecin

--- pages/admin/editmedecin.php
+++ pages/admin/editmedecin.php
@@ -1,19 +1,49 @@
 <?php
-    require_once '../classes/ConnexionDB.php';
-    require_once '../classes/Requette.php';
-    require_once '../classes/Formulaire.php';
-    if(isset($_GET['id'])){
-        $id =$_GET['id'];
-        $forms = new Requette();
-        $result = $forms->selectOne('medecin','id_medecin',$id);
-        
-          echo $result['id_service'];
-        
-       
+  require_once '../classes/ConnexionDB.php';
+  require_once '../classes/Requette.php';
+  require_once '../classes/Formulaire.php';
+  if(isset($_GET['id'])){
+  $id =$_GET['id'];
+  $req = new Requette();
+  $result = $req->selectOne('medecin','id_medecin',$id);
+  if(isset($_POST['submit'])){
+    $prenom = $_POST['prenom'];
+    $nom = $_POST['nom'];
+    $telephone = $_POST['telephone'];
+    $specialite = $_POST['specialite'];
+    $email = $_POST['email'];
+    $mdp = sha1($_POST['mdp']);
+    $service [] = $_POST['services'];
+    if(!empty($prenom) && !empty($nom) && !empty($telephone) && !empty($email) && !empty($mdp) && !empty($service) && !empty($specialite)){
+        if(preg_match('#^(77||78||76||70)[0-9]{7}$#',  $telephone)){
+            if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+              foreach ($service as $value) {
+                $value = intval($value);
+                $donnees = ['prenom'=>$prenom, 'nom'=>$nom, 'telephone'=>$telephone, 'specialite'=>$specialite,'email'=>$email, 'mdp'=>$mdp,'id_service'=>$value];
+                $add = new Requette();
+                $res =  $add->update($donnees,$id,'medecin','id_medecin');
+                if($res){
+                  header('location:medecine.php');
+                }else{
+                    echo 'impossible';
+                }
+              } 
+
+            }else{
+                $errormail = "<p class=\alert alert-danger \ role=\alert\"> Veuillez entrez une adresse email valide. </p>";
+            }
+        }else{
+            $errornumb = "<p class=\"alert alert-danger \" role=\"alert\"> Veuillez entrez un numéro de téléphone valide. </p>";
+        }
+    }else{
+        $errochamp = "<p class=\"alert alert-danger \" role=\"alert\"> Veuillez remplir tous les champs . </p>";
     }
+  } 
+}
+
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -22,69 +52,60 @@
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.9.0/css/all.css">
   <link rel="stylesheet" href="../../css/menu.css">
-  <title>MEDECINE</title>
-</head>
+  <title>MODIFIER UN SECRETAIRE</title>
 <body>
-<nav class="navbar navbar-inverse">
+    <nav class="navbar navbar-inverse">
             <div class="container-fluid">
               <ul class="nav navbar-nav">
                 <li class="active"><a href="homeadmin.php">ACCUEIL</a></li>
                 <li><a href="secretariat.php">SECRETARIAT</a></li>
                 <li><a href="medecine.php">MEDECINE</a></li>
                 <li><a href="services.php">SERVICES</a></li>
-              </ul>
-              <ul class="nav navbar-nav navbar-right">
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
                 <li><a href="deconnectadmin.php"><span class="glyphicon glyphicon-log-in"></span> DECONNEXION</a></li>
               </ul>
             </div>
           </nav>
-
-
-</body>
 <div class="container-fluid">
   
   <div class="panel-group col-md-4">
     <div class="panel panel-primary">
-      <div class="panel-heading">Modifier un medecin</div>
+      <div class="panel-heading">Modifier un sécretaire</div>
       <div class="panel-body">
+      <form action="" method="post">
         <?php
         if(isset($errormail)){echo $errormail;}
         if(isset($errornumb)){echo $errornumb;}
         if(isset($errochamp)){echo $errochamp;} 
         ?>
-        <form action="" method="post">
         <div class="form-group ">
-        <div class="form-group ">
-          <label>prenom</label>
-          <input type="text" class="form-control col-md-2" name="prenom" value="<?php if($result['prenom']) {echo $result['prenom'];} ?>" >
+            <?php
+                $forms = new Formulaire();
+                $req = new Requette();
+                echo $forms->formInput('Prenom','text','prenom','Entrez le prénom',$result['prenom']);
+                echo $forms->formInput('Nom ','text','nom','Entrez le nom ',$result['nom']);
+                echo $forms->formInput('Telephone','tel','telephone','Entrez le numero de telephone',$result['telephone']);
+                echo $forms->formInput('Specialite','text','specialite','Entrez le specialite',$result['specialite']);
+                echo $forms->formInput('Adresse email','email','email','Entrez l\'adresse email',$result['email']);
+                echo $forms->formInput('','hidden','mdp','Entrez le mot de passe',$result['mdp']);
+                $res = $req->selectAll('services');
+                $liste_option= "";
+                foreach ($res as $value) {
+
+                    $liste_option .= "<option value=".$value['id_service'].">" .$value['id_service'].' - '.$value['nom_service']."</option>";
+                }
+                echo $forms->selectList('Choisissez un service','services',$liste_option);
+            ?>
         </div>
-        <div class="form-group ">
-          <label>nom</label>
-          <input type="text" class="form-control col-md-2" name="nom" value="" >
-        </div>
-        <div class="form-group ">
-          <label>telephone</label>
-          <input type="text" class="form-control col-md-2" name="telephone" value="" >
-        </div>
-        <div class="form-group ">
-          <label>specialite</label>
-          <input type="text" class="form-control col-md-2" name="specialite" value="" >
-        </div>
-        <div class="form-group ">
-          <label>email</label>
-          <input type="text" class="form-control col-md-2" name="email" value="" >
-        </div>
+
+        <?php echo $forms->formSubmit('submit','Modifier'); ?>
+   
         
-        <div class="form-group">
-            <label >$label</label>
-            <select name="services" class="form-control col-md-4">
-                <option value='<?php if($result['id_service'] == $id) {echo 'selected';} ?>'></option>
-                <option value='<?php if($result['id_service'] == $id) {echo 'selected';} ?>'></option>
-                <option value='<?php if($result['id_service'] == $id) {echo 'selected';} ?>'></option>
-                <option value='<?php if($result['id_service'] == $id) {echo 'selected';} ?>'></option>
-            </select>
-        </div>
+      </form>
       </div>
-      </form>  
+ 
     </div>
   </div>
+</body>
+</html>
